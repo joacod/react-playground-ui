7c7adde52468fe8a8ced8e1b9c26b602c862515d
Joaquin Diaz <joacod@gmail.com>
2020-12-17 15:54:32 -0300

    change of web api url to net core

--- src/lyrics/Lyrics.js
+++ src/lyrics/Lyrics.js
@@ -29,10 +29,10 @@ const Lyrics = () => {
 
   const getLyrics = async function (artist, song) {
     // .Net Core Web API
-    //const lyricsUrl = 'https://localhost:44309/api/v1/lyrics/';
+    const lyricsUrl = "https://localhost:44309/api/v1/lyrics/";
 
     // Go Web API
-    const lyricsUrl = "http://localhost:8080/api/v1/lyrics/";
+    //const lyricsUrl = "http://localhost:8080/api/v1/lyrics/";
 
     await axios
       .get(lyricsUrl + artist + "/" + song)
